84f120bf527c91295da3363bbf5e4bcce56044fa
JUNG Myoungki <myoungki.jung@gmail.com>
2018-12-09 18:52:52 +1000

    Git init for PathPlanning and  Minkowski

---
+++ MinkowskiSum/README.md
@@ -0,0 +1,4 @@
+# Minkowski calculation
+
+[minkowski blog ](twistedoakstudios.com/blog/Post554_minkowski-sums-and-differences)
+[c++ stackexchange duplicates](https://stackoverflow.com/questions/1041620/whats-the-most-efficient-way-to-erase-duplicates-and-sort-a-vector)
---
+++ MinkowskiSum/src/mkski.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Print 2D vectors coordinate values
+void
+print2DVector (vector < vector < int > >vec)
+{
+  // Sorting the vector for grading purpose
+  sort (vec.begin (), vec.end ());
+  for (int i = 0; i < vec.size (); ++i)
+    {
+      for (int j = 0; j < vec[0].size (); ++j)
+	{
+	  cout << vec[i][j] << "  ";
+	}
+      cout << endl;
+    }
+}
+
+// ***TODO: Check for duplicate coordinates inside a 2D vector and delete them*** //
+vector < vector < int >>
+delete_duplicate (vector < vector < int > >C)
+{
+  vector < vector < int >>R;
+  // set < int >s (vec.begin (), vec.end ());
+  //R.assign (s.begin (), s.end ());
+
+  R.assign (C.begin (), C.end ());
+  std::sort (R.begin (), R.end ());
+  R.erase (std::unique (R.begin (), R.end ()), R.end ());
+
+  return R;
+
+}
+
+// ***TODO: Compute the Minkowski Sum of two vectors***//
+vector < vector < int >>
+minkowski_sum (vector < vector < int > >A, vector < vector < int > >B)
+{
+
+  vector < vector < int >>C;
+
+  for (int i = 0; i < A.size (); ++i)
+    {
+      for (int j = 0; j < B.size (); ++j)
+	{
+	  vector < int >R = { A[i][0] + B[j][0], A[i][1] + B[j][1] };
+	  C.push_back (R);
+	}
+    }
+
+  C = delete_duplicate (C);
+  return C;
+}
+
+int
+main ()
+{
+  // ***TODO: Define the coordinates of triangle A and B using 2D vectors*** //
+  vector < vector < int >>A;
+  A = { { 1, 0}, { 0, 1}, { 0, -1},};
+  vector < vector < int >>B;
+  B = { { 0, 0}, { 1, 1}, { 1, -1},}; 
+  // Compute the minkowski sum of triangle A and B
+  vector < vector < int >>C;
+  C = minkowski_sum (A, B);
+
+  // Print the resulting vector
+  print2DVector (C);
+
+  return 0;
+}
